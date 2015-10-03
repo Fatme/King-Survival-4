ff3d4913e5f0a4a2f1ce4fcb60731c9cebb04a88
Aleksandra Stojcheva <astojceva@yahoo.com>
2015-10-03 22:20:39 +0300

    Added tests for validator class

--- KingSurvivalNew/UnitTests/ValidatorTests.cs
+++ KingSurvivalNew/UnitTests/ValidatorTests.cs
@@ -1,4 +1,6 @@
-﻿namespace UnitTests
+﻿using KingSurvival.Figures;
+
+namespace UnitTests
 {
     using System;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -56,5 +58,45 @@
 
             Validator.ValidateGameInitialization(list, board);
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void CheckIfTheValidateGameInitializationMethodThrowsCorrectlyInvalidOperationExceptionWhenPlayersAndBoardAreIncorrect()
+        {
+            var adrian = new KingPlayer("Adrian");
+            var martin = new KingPlayer("Martin");
+            var blajev = new KingPlayer("Blajev");
+
+            var board = new Board(10,10);
+
+            var list = new List<IPlayer>();
+
+            list.Add(adrian);
+            list.Add(martin);
+            list.Add(blajev);
+
+            Validator.ValidateGameInitialization(list, board);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CheckIfCheckFigureOnTheWayWorksProperlyWhenFigureIsOnTheWay()
+        {
+            var board=new Board();
+            var figure = new Figure();
+            var position=new Position(1,1);
+            board.AddFigure(figure,position);
+            Validator.CheckIfFigureOnTheWay(position,board,"Figure on the way");
+        }
+        [TestMethod]
+        public void CheckIfCheckFigureOnTheWayWorksProperlyWhenNoFigureOnTheWay()
+        {
+            var board = new Board();
+            var figure = new Figure();
+            var position = new Position(1, 1);
+            board.AddFigure(figure, position);
+            var expectedPosition=new Position(2,2);
+            Validator.CheckIfFigureOnTheWay(expectedPosition, board, "No figure on the way");
+        }
+
     }
 }
