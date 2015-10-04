9023ec5236a2ed03a22153a11c7e536f978b4372
havaliova <havaliova@gmail.com>
2015-10-04 23:04:13 +0300

    Remove repeated code from engine

--- KingSurvivalNew/King-Survival/Engine/KingSurvivalEngine.cs
+++ KingSurvivalNew/King-Survival/Engine/KingSurvivalEngine.cs
@@ -25,6 +25,8 @@
         private readonly IInputProvider provider;
         private readonly IWinningConditions winningConditions;
         private IList<IPlayer> players;
+        private IPlayer kingPlayer;
+        private IPlayer pawnPlayer;
         
         private int currentPlayerIndex;
         private BoardMemory memory=new BoardMemory();
@@ -41,38 +43,18 @@
         {
             //TODO:Extract to another class and interface
             this.players = this.CreatePlayers();
+            this.kingPlayer = this.players[0];
+            this.pawnPlayer = this.players[1];
             Validator.ValidateGameInitialization(this.players, this.Board);
-            var firstPlayer = players[0];
-            var secondPlayer = players[1];
 
-            //TODO:Coupling between factory and this class :(
             IFigure figure = new Figure();
 
-            var king = figure.Clone();
-            king.AddSign(FigureSign.K);
-            firstPlayer.AddFigure(king);
-            this.Board.AddFigure(king, new Position(Constants.InitialKingRow, Constants.InitialKingColumn));
+            this.AddFigure(figure, FigureSign.K, new Position(Constants.InitialKingRow, Constants.InitialKingColumn));
+            this.AddFigure(figure, FigureSign.A, new Position(Constants.PawnAInitialRow, Constants.PawnAInitialCol));
+            this.AddFigure(figure, FigureSign.B, new Position(Constants.PawnBInitialRow, Constants.PawnBInitialCol));
+            this.AddFigure(figure, FigureSign.C, new Position(Constants.PawnCInitialRow, Constants.PawnCInitialCol));
+            this.AddFigure(figure, FigureSign.D, new Position(Constants.PawnDInitialRow, Constants.PawnDInitialCol));
 
-            var pawnA = figure.Clone();
-            pawnA.AddSign(FigureSign.A);
-            secondPlayer.AddFigure(pawnA);
-            this.Board.AddFigure(pawnA, new Position(Constants.PawnAInitialRow, Constants.PawnAInitialCol));
-
-            var pawnB = figure.Clone();
-            pawnB.AddSign(FigureSign.B);
-            secondPlayer.AddFigure(pawnB);
-            this.Board.AddFigure(pawnB, new Position(Constants.PawnBInitialRow, Constants.PawnBInitialCol));
-           
-            var pawnC = figure.Clone();
-            pawnC.AddSign(FigureSign.C);
-            secondPlayer.AddFigure(pawnC);
-            this.Board.AddFigure(pawnC, new Position(Constants.PawnCInitialRow, Constants.PawnCInitialCol));
-            
-            var pawnD = figure.Clone();
-            pawnD.AddSign(FigureSign.D);
-            secondPlayer.AddFigure(pawnD);
-            this.Board.AddFigure(pawnD, new Position(Constants.PawnDInitialRow, Constants.PawnDInitialCol));
-            
             this.SetFirstPlayerIndex();
           
             this.renderer.RenderBoard(this.Board);
@@ -122,6 +104,23 @@
                     this.HandleException(this.Board, ex.Message);
                 }
             }
+        }
+
+        private void AddFigure(IFigure figure, FigureSign figureSing, Position position)
+        {
+            var newFigure = figure.Clone();
+            newFigure.AddSign(figureSing);
+
+            if (figureSing == FigureSign.K)
+            {
+                this.kingPlayer.AddFigure(newFigure);
+            }
+            else
+            {
+                this.pawnPlayer.AddFigure(newFigure);
+            }
+
+            this.Board.AddFigure(newFigure, position);
         }
 
         private void HandleException(IBoard board, string message)
